6817745216ccab8b8e4593376e042258778e7d17
iaraedelstein <edeliaru@gmail.com>
2021-07-23 10:29:56 -0300

    cateogorías

--- src/components/CategoriaList/index.js
+++ src/components/CategoriaList/index.js
@@ -2,22 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { getCategories } from '../../services/categoryService';
 //import { useDispatch, useSelector} from 'react-redux';
 
-export default function ObtenerCategorias() {
+export default function CategoriaList() {
   const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    const categories = getCategories();
+  const fetchData = async () => {
+    const categories = await getCategories();
     setCategories(categories);
+  };
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
   return (
-    <div className="ejemplo">
-      <h1>Aca deberian estar las categorias</h1>
-      {categories.map((cat) => (
-        <div classname="idCategoria" key={cat.id}>
-          <p className="nombreCategoria">{cat.nombre}</p>
-        </div>
-      ))}
+    <div className="categories">
+      <h1>Categorías</h1>
+      {categories.map((cat) => {
+        return (
+          <div className="category-wrapper" key={cat.id}>
+            <p className="category--name">{cat.nombre}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
